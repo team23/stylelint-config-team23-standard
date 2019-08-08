9a902f3279a961e912be61efe75dd75c359d261a
Manuel Munz <munz@comuno.net>
2019-08-08 10:56:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stylelint-config-team23-standard
+
+> Standard CSS config for TEAM23
+
+Extends [`stylelint-config-standard`](https://github.com/stylelint/stylelint-config-standard).
---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "declaration-no-important": true,
+    "max-empty-lines": 2,
+    "no-descending-specificity": null,
+    "at-rule-no-vendor-prefix": true,
+    "property-no-vendor-prefix": true,
+    "selector-no-vendor-prefix": true,
+    "value-no-vendor-prefix": true,
+    "string-quotes": "single",
+    "function-url-quotes": "always",
+    "font-family-name-quotes": "always-where-recommended",
+    "selector-attribute-quotes": "always",
+    "max-nesting-depth": [
+      3,
+      {
+        "ignore": [
+          "pseudo-classes"
+        ]
+      }
+    ],
+    "selector-max-compound-selectors": 3,
+    "selector-class-pattern": [
+      "^[a-z0-9\\-_]+$",
+      {
+        "message": "Selector should be written in lowercase with hyphens or underscores (selector-class-pattern)"
+      }
+    ]
+  },
+}
