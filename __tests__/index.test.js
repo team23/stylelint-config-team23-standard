6bb484967bc81a362c8f689b811a53dc441d8ec7
Erik Herberg <herberg@team23.de>
2025-01-10 14:41:24 +0100

    test: improve tests for stylint errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,6 +8,25 @@ import stylelint from 'stylelint';
 const validCss = fs.readFileSync('./__tests__/css-valid.css', 'utf-8');
 const invalidCss = fs.readFileSync('./__tests__/css-invalid.css', 'utf-8');
 
+const expectedWarnings = [
+    'Unexpected at-rule "debug" (at-rule-disallowed-list)',
+    'Unexpected unknown at-rule "@debug" (at-rule-no-unknown)',
+    'Unexpected !important (declaration-no-important)',
+    'Unexpected !important (declaration-no-important)',
+    'Unexpected !important (declaration-no-important)',
+    'Unexpected !important (declaration-no-important)',
+    'Selector should be written in lowercase with hyphens or underscores (selector-class-pattern)',
+    'Selector should be written in lowercase with hyphens or underscores (selector-class-pattern)',
+    'Selector should be written in lowercase with hyphens or underscores (selector-class-pattern)',
+    'Selector should be written in lowercase with hyphens or underscores (selector-class-pattern)',
+    'Expected ".header__nav-link .icon .svg .path" to have no more than 3 compound selectors (selector-max-compound-selectors)',
+    'Expected ".item" to have no more than 3 compound selectors (selector-max-compound-selectors)',
+    'Expected ".link" to have no more than 3 compound selectors (selector-max-compound-selectors)',
+    'Expected ".footer .container .row .col .content" to have no more than 3 compound selectors (selector-max-compound-selectors)',
+    'Expected "#main-header" to have no more than 0 ID selectors (selector-max-id)',
+    'Expected "#header #nav" to have no more than 0 ID selectors (selector-max-id)',
+];
+
 describe('flags no warnings with valid css', () => {
     let result;
 
@@ -37,33 +56,20 @@ describe('flags warnings with invalid css', () => {
         });
     });
 
-    it('did error', () => result.then(
-        data => expect(data.errored).toBeTruthy(),
+    it('should flag as errored', () => result.then(data => {
+        expect(data.errored).toBeTruthy();
+    }));
+
+    it('should flag the correct number of warnings', () => result.then(
+        data => {
+            const expectedWarningCount = 16;
+            expect(data.results[0].warnings.length).toBe(expectedWarningCount);
+        },
     ));
 
-    it('flags one warning', () => result.then(
-        data => expect(data.results[0].warnings).toHaveLength(1),
-    ));
-
-    it('correct warning text', () => result.then(
-        data => expect(data.results[0].warnings[0].text).toBe(
-            'Expected ".a .b .c .d .e .f .g .h" to have no more than 3 compound selectors (selector-max-compound-selectors)',
-        ),
-    ));
-
-    it('correct rule flagged', () => result.then(
-        data => expect(data.results[0].warnings[0].rule).toBe('selector-max-compound-selectors'),
-    ));
-
-    it('correct severity flagged', () => result.then(
-        data => expect(data.results[0].warnings[0].severity).toBe('error'),
-    ));
-
-    it('correct line number', () => result.then(
-        data => expect(data.results[0].warnings[0].line).toBe(1),
-    ));
-
-    it('correct column number', () => result.then(
-        data => expect(data.results[0].warnings[0].column).toBe(1),
-    ));
+    it.each(expectedWarnings)('should contain warning: "%s"', async expectedWarning => {
+        const _result = await result;
+        const warnings = _result.results[0].warnings.map(warning => warning.text);
+        expect(warnings).toContain(expectedWarning);
+    });
 });
